773a9ead1d851e873f45cd0f5287f9557b787f7b
JacobWLorenzo <jacob.w.lorenzo@maine.edu>
2025-02-16 22:42:49 -0500

    COS_542: Updated Code to make it cleaner
    Changes to be committed:
    modified:   monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,112 +3,109 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define NumThreads 4
-#define NumPoints 1000000
-int Total_Hits = 0;
-pthread_mutex_t MyMutex;
-pthread_t ThreadID[NumThreads];
 
-void *Compute_Random(void *);
-void *Compute_Rand_r(void *);
+#define NUM_THREADS 4
+#define NUM_POINTS 1000000
 
-double Square_Num(double a) { return a * a; }
+int total_hits = 0;
+pthread_mutex_t mutex;
+pthread_t thread_id[NUM_THREADS];
 
-double Rand_Double(double max, double min) {
-  double Rand_Num = (double)random();
-  double Normalize = Rand_Num / (double)RAND_MAX;
-  double Shift_Range = Normalize * (max - min) + min;
-  return Shift_Range;
+void *compute_random(void *);
+void *compute_rand_r(void *);
+
+double square_num(double a) {
+    return a * a;
 }
 
-double Rand_r_Double(double max, double min, int *rand_seed) {
-  double Rand_Num = (double)rand_r(rand_seed);
-  double Normalize = Rand_Num / (double)RAND_MAX;
-  double Shift_Range = Normalize * (max - min) + min;
-  // printf("Shifted Range: %lf",Shift_Range);
-  return Shift_Range;
+double rand_double(double max, double min) {
+    double rand_num = (double)random();
+    double normalize = rand_num / (double)RAND_MAX;
+    double shift_range = normalize * (max - min) + min;
+    return shift_range;
+}
+
+double rand_r_double(double max, double min, int *rand_seed) {
+    double rand_num = (double)rand_r(rand_seed);
+    double normalize = rand_num / (double)RAND_MAX;
+    double shift_range = normalize * (max - min) + min;
+    return shift_range;
 }
 
 int main(int argc, char *argv[]) {
-  int ret;
-  int Count = NumPoints / NumThreads;
-  double result;
-  int Rand_Mode = 0;
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-alt") == 0) {
-      Rand_Mode = 1;
+    int ret;
+    int count = NUM_POINTS / NUM_THREADS;
+    double result;
+    int rand_mode = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-alt") == 0) {
+            rand_mode = 1;
+        }
     }
-  }
 
-  pthread_mutex_init(&MyMutex, NULL);
+    pthread_mutex_init(&mutex, NULL);
 
-  if (Rand_Mode == 1) {
-    for (int i = 0; i < NumThreads; i++) {
-      ret = pthread_create(&ThreadID[i], NULL, Compute_Rand_r, (void *)&Count);
+    if (rand_mode == 1) {
+        for (int i = 0; i < NUM_THREADS; i++) {
+            ret = pthread_create(&thread_id[i], NULL, compute_rand_r, (void *)&count);
+        }
+    } else {
+        for (int i = 0; i < NUM_THREADS; i++) {
+            ret = pthread_create(&thread_id[i], NULL, compute_random, (void *)&count);
+        }
     }
-  } else {
-    for (int i = 0; i < NumThreads; i++) {
-      ret = pthread_create(&ThreadID[i], NULL, Compute_Random, (void *)&Count);
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(thread_id[i], NULL);
     }
-  }
 
-  for (int i = 0; i < NumThreads; i++) {
-    pthread_join(ThreadID[i], NULL);
-  }
+    double pi_approx = ((double)total_hits / (double)NUM_POINTS) * 4.0;
 
-  double Pi_Approx = ((double)Total_Hits / (double)NumPoints) * 4.0;
+    printf("%lf\n", pi_approx);
 
-  printf("%lf\n", Pi_Approx);
-
-  return 0;
+    return 0;
 }
 
-void *Compute_Random(void *My_Count) {
-  int count = *((int *)My_Count);
-  int Num_Hits = 0;
-  unsigned int Rand_Seed = time(NULL) ^ (long)pthread_self();
-  srandom(Rand_Seed);
-  for (int i = 0; i < count; i++) {
+void *compute_random(void *my_count) {
+    int count = *((int *)my_count);
+    int num_hits = 0;
+    unsigned int rand_seed = time(NULL) ^ (long)pthread_self();
+    srandom(rand_seed);
 
-    double random_X = Rand_Double(1.0, -1.0);
-    double random_Y = Rand_Double(1.0, -1.0);
+    for (int i = 0; i < count; i++) {
+        double random_x = rand_double(1.0, -1.0);
+        double random_y = rand_double(1.0, -1.0);
 
-    if ((Square_Num(random_X) + Square_Num(random_Y)) <= 1) {
-      Num_Hits += 1;
+        if ((square_num(random_x) + square_num(random_y)) <= 1) {
+            num_hits += 1;
+        }
     }
-  }
 
-  pthread_mutex_lock(&MyMutex);
+    pthread_mutex_lock(&mutex);
+    total_hits += num_hits;
+    pthread_mutex_unlock(&mutex);
 
-  Total_Hits += Num_Hits;
-
-  pthread_mutex_unlock(&MyMutex);
-
-  return ((void *)NULL);
+    return ((void *)NULL);
 }
 
-void *Compute_Rand_r(void *My_Count) {
-  unsigned int Rand_Seed = time(NULL) ^ (long)pthread_self();
-  int count = *((int *)My_Count);
-  int Num_Hits = 0;
+void *compute_rand_r(void *my_count) {
+    unsigned int rand_seed = time(NULL) ^ (long)pthread_self();
+    int count = *((int *)my_count);
+    int num_hits = 0;
 
-  // printf("count: %d\n",count);
-  // printf("Rand_Seed: %d\n",Rand_Seed);
+    for (int i = 0; i < count; i++) {
+        double random_x = rand_r_double(1.0, -1.0, &rand_seed);
+        double random_y = rand_r_double(1.0, -1.0, &rand_seed);
 
-  for (int i = 0; i < count; i++) {
-
-    double random_X = Rand_r_Double(1.0, -1.0, &Rand_Seed);
-    double random_Y = Rand_r_Double(1.0, -1.0, &Rand_Seed);
-
-    if ((Square_Num(random_X) + Square_Num(random_Y)) <= 1) {
-      Num_Hits += 1;
+        if ((square_num(random_x) + square_num(random_y)) <= 1) {
+            num_hits += 1;
+        }
     }
-  }
 
-  pthread_mutex_lock(&MyMutex);
-  Total_Hits += Num_Hits;
+    pthread_mutex_lock(&mutex);
+    total_hits += num_hits;
+    pthread_mutex_unlock(&mutex);
 
-  pthread_mutex_unlock(&MyMutex);
-
-  return ((void *)NULL);
+    return ((void *)NULL);
 }
